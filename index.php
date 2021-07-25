634a14ec369faf58d19c18e9142d9eba5e848c93
Ronaldo Michuy Valera <66487844+Website14-rnmv@users.noreply.github.com>
2021-07-25 14:19:11 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,211 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <!--METATAGS-->
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport"
+        content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta name="IsosWare" content="Solución tecnológica de los Sistemas Integrados de Gestión">
+    <meta name="Description"
+        content="Mejora tu administracion con una solucion digital que te permitirá monitorear tu Sistema de Gestión de Seguridad y Salud en el Trabajo desde tu Smartphone permitiendo optimizar el trabajo de tus Jefes de SSOMAS y SST - ISO's Ware.">
+    <meta mame="Keywords"
+        content="ISO 37001, APP-ISO's Ware, Sistemas Integrados de Gestión, Gestion de Normas ISO, previene multas de SUNAFIL">
+    <meta name="author" content="ISO's ware">
+    <!--METATAGS-->
+
+    <!--LINKS ANCLADOS-->
+    <link rel="icon" type="image/png" href="icon.png">
+    <link rel="stylesheet" href="css/index.css">
+    <link rel="stylesheet" href="css/header.css">
+    <link rel="stylesheet" href="css/footer.css">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link
+        href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap"
+        rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"
+        integrity="sha512-iBBXm8fW90+nuLcSKlbmrPcLa0OT92xO1BIsZ+ywDWZCvqsWgccV3gFoRBv0z+8dLJgyAHIhR35VZc2oM/gI1w=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <title>Diagnostico de cumplimiento ISO's - ISO's Ware</title>
+</head>
+
+<body>
+    <header class=" ">
+        <nav>
+            <div class="logo">
+                <div class="cajon-1">
+                    <a href="../index.html"><img src="images/loo-ware-2.png" alt="ISO'S PERU LOGOTIPO" srcset=""></a>
+                    <div class="btn-sup">
+                        <i class="fas fa-th"></i>
+                    </div>
+                </div>
+                <div class="sombra show">
+                    <div class="closed">
+                        <i class="fas fa-times"></i>
+                    </div>
+                    <ul class="lista-1">
+                        <li class="hover-anclas no-activo-img"><img src="images/logo-hori.png" alt=""
+                                class="no-activo-img"></li>
+                        <li class="active hover-anclas"><a href="index.html">Inicio</a></li>
+                        <li class="hover-anclas"><a href="view/nosotros.html">Nosotros</a></li>
+                        <li class="hover-anclas"><a href="view/soluciones.html">Soluciones</a></li>
+                        <li class="hover-anclas"><a href="view/recursos.html">Recursos</a></li>
+                        <li><a target="_blank" class="calculadora"
+                                href="https://isosware.com/autodiagnostico/">Calculadora</a></li>
+                        <li><a class="acceder" href="index.html">Acceder</a></li>
+                    </ul>
+
+                </div>
+            </div>
+        </nav>
+    </header>
+
+    <main>
+        <div class="title-plataforma">
+            <h1>Diagnostico de cumplimiento ISO's</h1>
+            <p>Mediante este diagnostico en línea  te permitirá conocer el actual estado de tu organización respecto al cumplimiento de la norma ISO</p>
+        </div>
+        <section class="widgets">
+            <a target="_blank" href="view/ISO37001view/index.php" class="wid-isos">
+                <div class="imagen-wid">
+                    <img src="images/imagen1.jpg" alt="">
+                </div>
+                <div class="text-wid">
+                    <div class="blur"></div>
+                    <h2 class="iso">ISO 37001 - 2016</h2>
+                    <h3 class="subiso">Sistema de Gestión Antisoborno</h3>
+                </div>
+            </a>
+
+            <a href="" class="wid-isos">
+                <div class="imagen-wid">
+                    <img src="images/imagen2.png" alt="">
+                </div>
+                <div class="text-wid">
+                    <div class="blur"></div>
+
+                    <h2 class="iso">ISO 27001 - 2018</h2>
+                    <h3 class="subiso">Sistema de Gestión Antisoborno</h3>
+                </div>
+            </a>
+
+            <a href="" class="wid-isos">
+                <div class="imagen-wid">
+                    <img src="images/imagen3.jpg" alt="">
+                </div>
+                <div class="text-wid">
+                    <div class="blur"></div>
+                    <h2 class="iso">ISO 37001 - 2016</h2>
+                    <h3 class="subiso">Sistema de Gestión Antisoborno</h3>
+                </div>
+            </a>
+
+            <a href="" class="wid-isos">
+                <div class="imagen-wid">
+                    <img src="images/imagen4.jpg" alt="">
+                </div>
+                <div class="text-wid">
+                    <div class="blur"></div>
+                    <h2 class="iso">ISO 14001 - 2015</h2>
+                    <h3 class="subiso">Sistema de Gestión Antisoborno</h3>
+                </div>
+            </a>
+
+            <a href="" class="wid-isos">
+                <div class="imagen-wid">
+                    <img src="images/imagen5.jpg" alt="">
+                </div>
+                <div class="text-wid">
+                    <div class="blur"></div>
+                    <h2 class="iso">ISO 50001 - 2018</h2>
+                    <h3 class="subiso">Sistema de Gestión Antisoborno</h3>
+                </div>
+            </a>
+
+            <a href="" class="wid-isos">
+                <div class="imagen-wid">
+                    <img src="images/imagen6.gif" alt="">
+                </div>
+                <div class="text-wid">
+                    <div class="blur"></div>
+                    <h2 class="iso">ISO 90001 - 2015</h2>
+                    <h3 class="subiso">Sistema de Gestión Antisoborno</h3>
+                </div>
+            </a>
+
+        </section>
+    </main>
+    
+    <footer>
+        <div class="foot">
+            <div class="wid-foot">
+                <div class="wid">
+                    <img class="img-foot" src="/images/logo-blanco.png" alt="">
+                    <p class="text-foot">En ISO's Ware nos encargamos de facilitar tu proceso gestionando tu Sistema de
+                        Seguridad y Salud en el Trabajo (SST). Adquiere esta poderosa solución tecnológica para tu
+                        empresa.</p>
+                </div>
+                <div class="wid widgets-center">
+                    <h1>Nuestros Servicios</h1>
+                    <ul class="lista-1-foot">
+                        <li class="anclas-li-foot"><a href="index.html">Inicio</a></li>
+                        <li class="anclas-li-foot"><a href="view/nosotros.html">Nosotros</a></li>
+                        <li class="anclas-li-foot"><a href="view/soluciones.html#caracter">Características</a></li>
+                        <li class="anclas-li-foot"><a href="view/preguntas.html">Preguntas Frecuentes</a></li>
+                        <li class="anclas-li-foot"><a href="view/blog.php">Blog</a></li>
+                    </ul>
+                </div>
+                <div class="wid widgets-center">
+                    <h1>Otros</h1>
+                    <ul class="lista-1-foot">
+                        <li class="anclas-li-foot"><a href="view/contacto.php">Contacto</a></li>
+                        <li class="anclas-li-foot"><a href="view/terminos.html">Términos y condiciones</a></li>
+                        <li class="anclas-li-foot"><a href="view/brochure.html">Brochure</a></li>
+                        <li class="anclas-li-foot"><a href="view/videos.html">Videos</a></li>
+                        <li class="anclas-li-foot"><a href="index.html">Actualizaciones</a></li>
+                    </ul>
+                </div>
+                <div class="wid">
+                    <h1>Contactanos</h1>
+                    <ul class="lista-1-foot">
+                        <li class="info-foot"><i class="fas fa-map-marker-alt"></i>
+                            <p>Av. Circunvalación del Golf Los Incas 208 Of. 604A</p>
+                        </li>
+                        <li class="info-foot"><i class="far fa-envelope"></i>
+                            <p>contacto@isosware.com</p>
+                        </li>
+                        <li class="info-foot"><i class="fas fa-mobile-alt"></i>
+                            <p>+51 937 527 944</p>
+                        </li>
+                        <a class="decargar-foot" href="view/contacto.php">Contactar</a>
+                    </ul>
+                </div>
+            </div>
+            <div class="pie-copy">
+                <div class="terminos">
+                    <p>Copyright © By ISO's Ware.</p>
+                    <p class="derechos">Todos los derechos reservados</p>
+
+
+                </div>
+                <div class="copy">
+                    <li class="redes-sociales">
+                        <a target="_blank" title="@Iso's Ware" href="https://www.facebook.com/isoswareapp/"><i
+                                class="fab fa-facebook-f"></i></a>
+                        <a target="_blank" title="ISO's Ware®" href="https://www.linkedin.com/company/iso-s-ware/"><i
+                                class="fab fa-linkedin"></i></a>
+                        <a target="_blank" title="@isos" href="https://www.instagram.com/isosware/"><i
+                                class="fab fa-instagram"></i></a>
+                        <a target="_blank" title="ISO's Ware YT"
+                            href="https://www.youtube.com/channel/UCf3l7JxwxIUZA6QWzfV1cXw"><i
+                                class="fab fa-youtube"></i></a>
+                    </li>
+                </div>
+            </div>
+        </div>
+    </footer> 
+
+</body>
+
+</html>
